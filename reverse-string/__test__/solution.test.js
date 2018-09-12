a331de51d4c5c70eb4c40816b4027190a9ccaf2c
jlhiskey <jasonhi@crazyredhead.com>
2018-09-12 16:48:43 -0700

    added tests

---
+++ reverse-string/__test__/solution.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const solution = require('../lib/solution');
+
+describe('#solution.js', () => {
+  test('Testing that arr is reversed', () => {
+    const output = solution.reverse_string(['1', '2', '3', '4']);
+    expect(output).toEqual(['4', '3', '2', '1']);
+  });
+  test('Testing that arr is populated', () => {
+    const output = solution.reverse_string([]);
+    expect(output).toEqual([null]);
+  });
+});
