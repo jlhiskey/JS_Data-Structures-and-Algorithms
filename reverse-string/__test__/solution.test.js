836eb6c193392909a5f274bf5ebbff5edf8d1ec3
jlhiskey <jasonhi@crazyredhead.com>
2018-09-19 13:31:38 -0700

    fixing a copied over solution

--- reverse-string/__test__/solution.test.js
+++ reverse-string/__test__/solution.test.js
@@ -3,12 +3,12 @@
 const solution = require('../lib/solution');
 
 describe('#solution.js', () => {
-  test('Testing that newArr is adding mid', () => {
-    const output = solution.reverse_string(['1', '2', '3', '4'], '9');
-    expect(output).toEqual(['1', '2', '9', '3', '4']);
+  test('Testing that arr is reversed', () => {
+    const output = solution.reverse_string(['1', '2', '3', '4']);
+    expect(output).toEqual(['4', '3', '2', '1']);
   });
-  test('Testing that arr is populated with more than 1 value', () => {
-    const output = solution.reverse_string(['1']);
+  test('Testing that arr is populated', () => {
+    const output = solution.reverse_string('');
     expect(output).toEqual(null);
   });
 });
