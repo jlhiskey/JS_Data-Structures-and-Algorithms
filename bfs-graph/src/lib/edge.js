62aed5a5854e4afcef2f9ff70cf07ab4a2d858a2
jlhiskey <jasonhi@crazyredhead.com>
2018-10-30 15:21:45 -0700

    completed challenge

---
+++ bfs-graph/src/lib/edge.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    // this.startNode = vertex;
+    // this.endNode = vertex;
+    this.weight = weight;
+  }
+};
